bd3ed06453f7179f2bc6bee151c5d3abaa00d646
Sunho, Bang <snoya00@gmail.com>
2015-06-21 16:44:26 +0900

    메시지 작성 시간 오류 수정

--- app/src/main/java/com/androidstudy/pushchat/MainActivity.java
+++ app/src/main/java/com/androidstudy/pushchat/MainActivity.java
@@ -22,6 +22,7 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class MainActivity extends ActionBarActivity {
@@ -90,8 +91,12 @@ public class MainActivity extends ActionBarActivity {
 
     private void initalizeTargetList()
     {
-        mTargetList.put("스노야", "APA91bFz0GSPUcBdQNTCfT4H83H8jGvWsboUA9RNb1MyObRnhNGI6lfZ8r_J9bgQgf15ha-zxfPSn-tCPhClHxkep9KWDAHmpmXDxhoPDywWln_VZHvQzfaR8JMf5GlMIs8v5ngTEW1CmyRI9mLR4AWGcB9OT3AJGA");
-//        mTargetList.put("멜트", "APA91bH0zkR6P0PtKboTE3xOOM5Ssib3tDFVrzHhBMM_d4iyWs_y9EsUurznqnipxKdbWGLjdgV2s4bK71ORl8_1bjqPmAon8AOMDGLugFYxK8BNEb759qdFuInmwupw7UQDLtErK4L57yPEYToKxvXjw006DS7O7A");
+        //mTargetList.put("스노야", "APA91bFf7n2S-O7W1lpTaSoQNqSCwoXinevIsc7NVdJUDpoMa4FQ8bbzW76wG8yIH8V_cddnCxCkkiG9Aev46vG5owea3Bys0jgx8KoTKQZ4S6fm0DAGVpSlR4Zv_sOdz7-k2Xa62KRVdlp0_SFf7pE0t1mPpScurQ");
+        mTargetList.put("멜트", "APA91bH0zkR6P0PtKboTE3xOOM5Ssib3tDFVrzHhBMM_d4iyWs_y9EsUurznqnipxKdbWGLjdgV2s4bK71ORl8_1bjqPmAon8AOMDGLugFYxK8BNEb759qdFuInmwupw7UQDLtErK4L57yPEYToKxvXjw006DS7O7A");
+        mTargetList.put("Gus", "APA91bE8DeJQJ0FC-5ROqsDkv4x1ZP39cjBiGc5oQyI0EiNdHVpBF3azWKam7drW0P2V2Xk5bJL3kTXTWa0zaPBaVDiZfR8e4_FjprspAg3KJAE4QA4-tYInn5wvjgZamcI31fEwCw2aW0Njw5m3S2_3KFHC90FgWg");
+        mTargetList.put("아이린", "APA91bFz0GSPUcBdQNTCfT4H83H8jGvWsboUA9RNb1MyObRnhNGI6lfZ8r_J9bgQgf15ha-zxfPSn-tCPhClHxkep9KWDAHmpmXDxhoPDywWln_VZHvQzfaR8JMf5GlMIs8v5ngTEW1CmyRI9mLR4AWGcB9OT3AJGA");
+        mTargetList.put("taiwan", "APA91bH1U8N8XfihoVG2Kq1GeQAjUkoFasQhAToeAsnWuMXrqIt46fEg4jRQGs3Z3AKfPXobpA8xlxuPKstX_QbTcRafl8RSUaePuXAAuTIIKLgI3SMll2ctPu1j-JzJ0n3OgWvpfuvO");
+        mTargetList.put("Foxy", "APA91bFJDJLGyocR4dskb5m7XK1l5XjAddHo3HCA3DEiszdUIRI54h4cW7iY4OK4TPpY7KNRpc35ukhjNTA9N6IPLUJJe_Lr7DVAYEV2jm7Gpfh73x2Sx_RXti31TGhsgxpXtuToMAdb0JBPCCg4356NasEw94jDgQ");
 //        mTargetList.put("에이든", "APA91bFDqjJ-N1EtEehf841I1Ha0nxkdi_iWnqr8sLx_h9X9wldhmCTj32WWRflb8Jkt_iJ_T2MHU6T6xcSZIVIJdaltlSdmlBB6wSmAM-PCgD0z2_vdG59VUYn6g1-Gqsc2r5jkDoW7Yx-6wqANzhaByiqtPKlO-A");
 //        mTargetList.put("권터", "APA91bGYEDi_ZPEC6T2lGPxUAls90ASSAqjDF2zB9lPYNmSKQMzh3gJssuyz04GxK1DQT7RNVSs5T3nbJme_VaTkXST0DI6y_baUtN4PMLcRHh8dj7dIU0vIZvMwUk5q1_6BB4u32F17PSlJSNhMLylhfOdqja0nNg");
 //        mTargetList.put("클로이", "APA91bGM9KAMb2aKJYUmbxp9_w-oRk7NKiWJR8FG7li4ScZq09CO8wscLpUDqkHI8TozLB0f3Jf1-5s-s2v9t9m6dDzR3myZtt0ugZ_I926vOxV2FaQwyUYZY7D-7ya5s23EklsnN6bJNyzDQaC0P2iUF_pEnqLl1Q");
@@ -99,8 +104,6 @@ public class MainActivity extends ActionBarActivity {
 //        mTargetList.put("emmily", "APA91bHj2GrBg8aOD3p4Q_zrh6-1L7kPamB5Nmo2s7ynFKzS2oZXiZlrQCWjmznEgQs0rDrN4-e7NIeejIHEmSTU6fkY44vpO-GFlagtq2gRRpPLxqNrGcIStOsYvy2Soiw3kq9ZYSB0SUejLgtNqnq672IX7gSIKw");
 //        mTargetList.put("홍(Hong)", "APA91bFiV1SG40xrYA54by0E0zuX2sgJdxJ5MU1mDXFS-Q2-WkGgfkMClk54DC0d2PoQ4vktZqcSE42zeeRTe36GpPO-FprmxA83KdbMIPl5mvvWT3hgEA4_9WWYRQlemqEK0OSijuL19F72x3jl7GBF1dj0oloh1g");
 //        mTargetList.put("Park", "APA91bHlyqeJdHWMnWcNzdpSk03KuaTNLhNFYcKiLm0pVWh4Iu8SqauU_fNNr04niIUtcrJUSZ8L0gysuQe9g2ncUBqx8AuWIxLthI7sIu3_OEhWWLSmwUEDF_37nFwroKcXi9Dx6qWE7YoR0wQQC7qqgUjACY6otQ");
-//        mTargetList.put("taiwan", "APA91bG9IK0Qn12QmtHGspfJkwu56Ngie23HOCqSHzGr7HOsEAjzPdGhayGrpt6vCwHRMxVBFxSOQlAxgVuhNZJV1UzbcuJnoXFXrP_iHzv8-VN4zTnqAhjTz5DV3bGyIWn_gWXqCE0O4liayQVM8nIm84Lxz8umcg");
-//        mTargetList.put("아이린", "APA91bGZ5z9htExdKbCpHOuOoArCykowqdqNQVE2lbkTXLpwUm3n7PttOF4Fa69AQM5qhMd4V5T3VMcnp501WsVQRaWpXA-xZfzRxp9kUYZkmBHfOf2iL9nyfcvgfn7664PombPfVAkbcVAN9-SEu-f4NwUPeQMWiQ");
 //        mTargetList.put("Geun", "APA91bHvTXBPJ3AUthtpgyRa9r8SNst8-PDxcov0r19vMtkVAXseJXdAqzlHt95_2iZtGO4TsCpdsJ5VAbZL2AfBd3xkKKt2aY3GYT-Tl4bklwW27Q1OmBD3YVrWHjwSs1ACEFUqgn408EbWsjm-5z5hJIVGA0HKnw");
 
         for (String name : mTargetList.keySet())
@@ -163,13 +166,23 @@ public class MainActivity extends ActionBarActivity {
     public static String API_KEY = "AIzaSyDKSIu5JIw_E27pAarcQDnSe2QAM4A8J48";
 
     public void sendPush(final String content) {
+        final TalkModel talk = new TalkModel();
+        talk.author = MY_NICK;
+        talk.created = new Date();
+        talk.content = content;
+        talk.my_talk = true;
+        MyApp.dbHelper.addTalk(talk);
+        addTalk(talk);
+        scrollDown();
+
         new AsyncTask() {
             @Override
             protected Object doInBackground(Object[] objects) {
                 Sender sender = new Sender(API_KEY);
                 Message message = new Message.Builder()
-                        .addData("author", MY_NICK)
-                        .addData("content", content)
+                        .addData("author", talk.author)
+                        .addData("created", CalUtil.dateToString(talk.created))
+                        .addData("content", talk.content)
                         .build();
                 try {
                     sender.send(message, mTargetRegIdList, 0);
@@ -179,14 +192,6 @@ public class MainActivity extends ActionBarActivity {
                 return null;
             }
         }.execute();
-
-        TalkModel talk = new TalkModel();
-        talk.author = MY_NICK;
-        talk.content = content;
-        talk.my_talk = true;
-        MyApp.dbHelper.addTalk(talk);
-        addTalk(talk);
-        scrollDown();
     }
 
     private void addTalk(TalkModel talk) {
